c77007513b91db6d894f7da46cd27c39b0b71fe1
Rayn <rayn.tariq@gmail.com>
2025-01-02 21:07:39 +0530

    Move error middleware call

--- server/app.ts
+++ server/app.ts
@@ -32,6 +32,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
 	const err = new Error(`Route ${req.originalUrl} not found`) as any;
 	err.statusCode = 404;
 	next(err);
-
-	app.use(ErrorMiddleware);
 });
+
+app.use(ErrorMiddleware);
